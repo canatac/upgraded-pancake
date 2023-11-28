657f50770790ab8855decd90f2f13f96d49da19b
Jan ATAC <can.atac@gmail.com>
2023-11-28 16:16:45 +0000

    change test value

--- src/main.rs
+++ src/main.rs
@@ -86,7 +86,7 @@ mod tests {
 
     #[test]
     fn test_check_expiration_valid_api_key() {
-        let days_before_expiration = 48; // Replace with the number of days before expiration
+        let days_before_expiration = 47; // Replace with the number of days before expiration
 
         let client = Client::tracked(rocket()).expect("valid rocket instance");
         let request_body = r#"{"url":"www.google.com","api_key":"valid_api_key"}"#;
